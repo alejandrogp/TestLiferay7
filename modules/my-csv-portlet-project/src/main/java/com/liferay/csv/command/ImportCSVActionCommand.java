61276ec74ed784779cb76de8a7b217a2b5c73e39
Alejandro García Pérez <alejandro.garcia@mimacom.com>
2017-04-03 16:40:09 +0200

    Import CSV

--- modules/my-csv-portlet-project/src/main/java/com/liferay/csv/command/ImportCSVActionCommand.java
+++ modules/my-csv-portlet-project/src/main/java/com/liferay/csv/command/ImportCSVActionCommand.java
@@ -22,13 +22,68 @@ import javax.portlet.PortletException;
 )
 public class ImportCSVActionCommand implements MVCActionCommand {
 
+    private static final Log _log = LogFactoryUtil.getLog(ImportCSVActionCommand.class);
+
     @Override
     public boolean processAction(ActionRequest actionRequest, ActionResponse actionResponse) throws PortletException {
 
+        ServiceContext serviceContext = ServiceContextFactory.getInstance(User.class.getName(), actionRequest);
+        PortletContext portletContext = actionRequest.getPortletSession().getPortletContext();
         ThemeDisplay themeDisplay = (ThemeDisplay) actionRequest.getAttribute(WebKeys.THEME_DISPLAY);
 
-        System.out.println("Hola " );
+        _log.debug(" :: Import CSV :: ");
+
+        String fileName = request.getFileName("file");
+
+        _log.debug("   File: " + fileName);
+
+        try {
+            File file = request.getFile("file");
+            CSVReader reader = new CSVReader(new InputStreamReader(new FileInputStream(file), StringPool.UTF8), CharPool.SEMICOLON);
+
+            //field1;field2;field3;field4;field5
+
+            String[] line;
+            while ((line = reader.readNext()) != null) {
+
+                try {
+
+                    String field1 = "";
+                    String field2 = "";
+                    String field3 = "";
+                    String field4 = "";
+                    String field5 = "";
+
+                    if (line.length > 0) {
+                        field1 = line[0];
+                    }
+                    if (line.length > 1) {
+                        field1 = line[1];
+                    }
+                    if (line.length > 2) {
+                        field1 = line[2];
+                    }
+                    if (line.length > 3) {
+                        field1 = line[3];
+                    }
+                    if (line.length > 4) {
+                        field1 = line[4];
+                    }
+
+
+
+
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return true;
     }
+
 }
